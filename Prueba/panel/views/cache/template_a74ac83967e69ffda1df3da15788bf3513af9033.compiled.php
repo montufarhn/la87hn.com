e53d5f4e513234e97f77351cbf26dd5063d93386
montufarhn <53841683+montufarhn@users.noreply.github.com>
2025-09-05 17:13:24 -0400

    new player

--- Prueba/panel/views/cache/template_a74ac83967e69ffda1df3da15788bf3513af9033.compiled.php
+++
@@ -1,7 +0,0 @@
-<?php echo $this->runChild( 'template.header' ); ?>
-<?php echo $this->runChild( 'template.navigation' ); ?>
-<?php 
- $panel->flashMessages();
- ?>
-<?php echo $this->yieldContent( 'content'); ?>
-<?php echo $this->runChild( 'template.footer' ); ?>
--- Prueba/panel/views/template.blade.php
+++
@@ -1,7 +0,0 @@
-@include( 'template.header' )
-@include( 'template.navigation' )
-@php
-    $panel->flashMessages();
-@endphp
-@yield( 'content')
-@include( 'template.footer' )
